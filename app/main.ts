a01a48237b96780389dc5c4334c8806de1989bee
TPriince <tobiloba110@gmail.com>
2024-07-11 10:11:32 +0100

    pass 3rd  stage

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,34 @@
 import * as net from "net";
 
+// const server = net.createServer((socket) => {
+//   socket.write("HTTP/1.1 200 OK\r\n\r\n");
+//   socket.on("close", () => {
+//     socket.end();
+//   });
+// });
+
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    // console.log(`Received data: ${data}`);
+
+    const entireData = data.toString().split(" ");
+    console.log({ entireData });
+    const urlPath = entireData[1];
+
+    socket.write(
+      urlPath === "/"
+        ? "HTTP/1.1 200 OK\r\n\r\n"
+        : "HTTP/1.1 404 Not Found\r\n\r\n"
+    );
+  });
+
   socket.on("close", () => {
     socket.end();
   });
 });
 
 server.listen(4221, "localhost");
+
+// server.listen(4221, () => {
+//   console.log("Server listening on port 4221");
+// });
