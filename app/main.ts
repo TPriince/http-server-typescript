f75d6b982ec86f0255d8f82cf909ebf418ce1f03
TPriince <tobiloba110@gmail.com>
2024-07-13 19:44:02 +0100

    fix: Read header (fix four)

--- app/main.ts
+++ app/main.ts
@@ -7,26 +7,35 @@ const server = net.createServer((socket) => {
     let userAgent = "";
 
     const entireData = data.toString().split("\r\n");
+    const entireDataTwo = data.toString().split(" ");
+    const urlPath = entireDataTwo[1];
+
+    console.log({ entireDataTwo });
+
     entireData.forEach((datum) => {
-      if (datum.includes("User-Agent:")) {
+      if (datum.toLocaleLowerCase().includes("user-agent:")) {
         console.log({ datum });
         const formattedText = datum.split(" ").slice(1).join("");
         userAgent = formattedText;
       }
     });
-    // const userAgentString = entireData[1];
-    // const userAgent = userAgentString.split(" ").slice(1).join(" ");
-    // // const userAgent = userAgentArray[1];
 
-    // console.log({ entireData });
+    console.log({ x: urlPath.split("/") });
 
-    // // console.log({ userAgentArray });
+    let response = "";
 
-    // console.log({ userAgent });
+    if (!userAgent) {
+      response = "HTTP/1.1 200 OK\r\n\r\n";
+    } else if (urlPath.split("/")[1] === "echo") {
+      const messageArray = urlPath.split("/");
+      const message = messageArray[messageArray.length - 1];
+      console.log({ message });
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
+    } else {
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+    }
 
-    socket.write(
-      `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
-    );
+    socket.write(response);
   };
 
   socket.on("data", readData);
