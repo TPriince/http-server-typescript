8697453c7d6fba14959855ca99a7f4fe53e7d22b
TPriince <tobiloba110@gmail.com>
2024-07-15 07:48:03 +0100

    feat: Read request body

--- app/main.ts
+++ app/main.ts
@@ -1,35 +1,54 @@
 import * as net from "net";
+import process from "process";
+import { readFileSync } from "fs";
 
 const server = net.createServer((socket) => {
-  const readData = (data: Buffer) => {
-    console.log(`Received data: ${data}`);
-
-    let userAgent = "";
-
-    const entireData = data.toString().split(" ");
-    const urlPath = entireData[1];
-
-    const entireDataTwo = data.toString().split("\r\n");
-
-    entireDataTwo.forEach((datum) => {
-      if (datum.toLocaleLowerCase().includes("user-agent:")) {
-        const formattedText = datum.split(" ").slice(1).join("");
-        userAgent = formattedText;
-      }
-    });
-
+  const setResponse = (data: Buffer, urlPath: string, userAgent: string) => {
+    const urlPathWithoutSlashes = urlPath.split("/");
+    const length = urlPathWithoutSlashes.length;
+    const path = urlPathWithoutSlashes[length - 1];
     const urlContent = urlPath.split("/")[1];
+
+    console.log({ path });
+    console.log({ urlPathWithoutSlashes });
 
     let response = "";
 
-    if (!urlContent) {
+    if (urlPath === "/") {
       response = "HTTP/1.1 200 OK\r\n\r\n";
     } else if (urlContent === "echo") {
       const messageArray = urlPath.split("/");
       const message = messageArray[messageArray.length - 1];
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
-    } else if (userAgent) {
+    } else if (urlPathWithoutSlashes[1] === "user-agent") {
+      const entireDataTwo = data.toString().split("\r\n");
+
+      // console.log({ entireDataTwo });
+
+      entireDataTwo.forEach((datum) => {
+        if (datum.toLocaleLowerCase().includes("user-agent:")) {
+          const formattedText = datum.split(" ").slice(1).join("");
+          userAgent = formattedText;
+        }
+      });
+
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+    } else if (urlPathWithoutSlashes[1] === "files") {
+      const directory = process.argv[2];
+      const filePath = directory + path;
+
+      console.log({ filePath });
+
+      try {
+        const fileContent = readFileSync(filePath);
+
+        console.log({ fileContent });
+        response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
+      } catch (error) {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+      }
+
+      response = "HTTP/1.1 200 OK\r\n\r\n";
     } else {
       response = "HTTP/1.1 404 Not Found\r\n\r\n";
     }
@@ -37,11 +56,28 @@ const server = net.createServer((socket) => {
     socket.write(response);
   };
 
+  const readData = (data: Buffer) => {
+    console.log(`Received data: ${data}`);
+
+    const entireData = data.toString().split(" ");
+    const urlPath = entireData[1];
+
+    // console.log({ urlPath });
+
+    let userAgent = "";
+
+    let response = ``;
+
+    setResponse(data, urlPath, userAgent);
+  };
+
+  const close = () => {
+    socket.end();
+  };
+
   socket.on("data", readData);
 
-  socket.on("close", () => {
-    socket.end();
-  });
+  socket.on("close", close);
 });
 
 // server.listen(4221, "localhost");
