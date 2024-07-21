1769c315c429a3ae5ae5383c5b5b4bc67d9042ca
TPriince <tobiloba110@gmail.com>
2024-07-21 15:27:00 +0100

    feat: Gzip compression (fix one)

--- app/main.ts
+++ app/main.ts
@@ -38,6 +38,8 @@ const server = net.createServer((socket) => {
         const zipped = zlib.gzipSync(buffer);
 
         response = `HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${zipped.length}\r\n\r\n${message}`;
+        socket.write(zipped);
+        return;
       } else {
         response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
       }
